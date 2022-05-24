bf726dc9e59d50819e1f35e810c9c81ec46cb73a
ensorrow <zheyanglv@qq.com>
2022-05-24 15:45:01 +0800

    fix: diff editor encoding switch

--- packages/editor/src/browser/editor.contribution.ts
+++ packages/editor/src/browser/editor.contribution.ts
@@ -751,8 +751,12 @@ export class EditorContribution
           return;
         }
 
-        this.editorDocumentModelService.changeModelOptions(resource.uri, {
-          encoding: selectedFileEncoding,
+        const uris =
+          resource.uri.scheme === 'diff' ? [resource.metadata.original, resource.metadata.modified] : [resource.uri];
+        uris.forEach((uri) => {
+          this.editorDocumentModelService.changeModelOptions(uri, {
+            encoding: selectedFileEncoding,
+          });
         });
       },
     });
